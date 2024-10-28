fd326a2937c6f136ac1a4825d056427a30edd265
Anthony <anthony@ilimitech.com>
2024-10-28 20:28:25 +0100

    base commit

---
+++ spring-boot-base/src/test/java/com/devfay/springbootbase/SpringBootBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfay.springbootbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
